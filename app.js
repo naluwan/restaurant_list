77d8d994d62734be389031eba4076303afc337a0
naluwan <street.baller06@gmail.com>
2021-05-17 17:19:43 +0800

    Add show restaurant page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -13,7 +14,14 @@ app.use(express.static('public'))
 
 //routes setting
 app.get("/", (req, res) => {
-  res.render('index')
+  console.log(restaurantList.results)
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+
+  res.render('show', { restaurant: restaurant })
 })
 
 
