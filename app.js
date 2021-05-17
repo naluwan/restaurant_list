029a75ed9afd794455099967e3afdaf7e94708b1
naluwan <street.baller06@gmail.com>
2021-05-17 16:32:49 +0800

    Init restaurant list project

---
+++ app.js
@@ -0,0 +1,26 @@
+// require packages used in the project
+const express = require("express")
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+//routes setting
+app.get("/", (req, res) => {
+  res.render('index')
+})
+
+
+
+
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is running on localhost:${port}`)
+})
