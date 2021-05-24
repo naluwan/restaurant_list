dab9e3fac9aef77c71f7242fd97a978316647d2b
naluwan <street.baller06@gmail.com>
2021-05-24 21:17:36 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -95,6 +95,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
 
