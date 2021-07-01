2d972855f48dd25f5b9a84bd1f458bc3c41c5ad6
naluwan <street.baller06@gmail.com>
2021-07-01 17:48:51 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // require packages used in the project
 const express = require("express")
+const session = require('express-session')
 const app = express()
 const PORT = 3000
 const hbs = require('express-handlebars')
@@ -16,8 +17,13 @@ require('./config/mongoose')
 app.engine('hbs', hbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-// setting static files
+// middleware
 app.use(express.static('public'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
