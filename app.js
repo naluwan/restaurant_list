390773dd02ded8e8f3b8e4b690170e6a6a088e1c
naluwan <street.baller06@gmail.com>
2021-07-02 17:37:57 +0800

    fix: modify message partials

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
-  res.locals.error = req.flash('error')
+  res.locals.errorMsg = req.flash('error')
   next()
 })
 app.use(routes)
